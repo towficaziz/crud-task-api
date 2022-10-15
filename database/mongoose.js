ec7971ff2062784860d886b3f8fa27e41d2f70b6
towficaziz <tapolly01@gmail.com>
2022-10-15 02:23:20 +0200

    added azur dbase link with commented

--- database/mongoose.js
+++ database/mongoose.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb+srv://root:root@cluster0.avfdqfj.mongodb.net/taskmanagerdb")
+ mongoose.connect("mongodb+srv://root:root@cluster0.avfdqfj.mongodb.net/taskmanagerdb")
+// mongoose.connect("mongodb://towficmeanstackdb:3zuX8LCWdlIJwvb5c3mZNVYARxXPxRS2ZUzS1u326NUBmA5FbuAH3gY4AheFLAB3kfxtgT1JORIj76aQQkXzYg==@towficmeanstackdb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@towficmeanstackdb@")
 // ,{useNewUrlParser: true, useUnifiedTopology: true}
 .then(()=>{
     console.log("Connected to D-Base!");
